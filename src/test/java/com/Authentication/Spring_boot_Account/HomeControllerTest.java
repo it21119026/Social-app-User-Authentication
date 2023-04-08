36aa9ca79c8427189063303d1ddfd88ae1cbbfb6
Kolitha <it21119026@my.sliit.lk>
2023-04-08 16:27:47 +0530

    new test

---
+++ src/test/java/com/Authentication/Spring_boot_Account/HomeControllerTest.java
@@ -0,0 +1,31 @@
+package com.Authentication.Spring_boot_Account;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.mockito.ArgumentMatchers.contains;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+class HomeControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void authenticatedUserCanDisplayHomePage() throws Exception {
+        mockMvc.perform(get("/")).andExpect(status().isOk());
+    }
+
+
+}
