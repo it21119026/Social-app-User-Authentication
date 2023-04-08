fa840ef59e59885de50bb70c563a24d120730abf
Kolitha <it21119026@my.sliit.lk>
2023-04-08 15:01:03 +0530

    added home controller

---
+++ Spring_boot_Account/src/test/java/com/Authentication/Spring_boot_Account/SpringBootAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Authentication.Spring_boot_Account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
