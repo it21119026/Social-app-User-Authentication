ff6079c6be52c73f362bfb9e6f3515f1f0bff136
Kolitha <it21119026@my.sliit.lk>
2023-04-12 12:48:56 +0530

    new envvv

---
+++ src/main/java/com/Authentication/Spring_boot_Account/security/AppUserService.java
@@ -0,0 +1,116 @@
+package com.Authentication.Spring_boot_Account.security;
+
+import jakarta.annotation.PostConstruct;
+import lombok.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+@Service
+@Value
+public class AppUserService implements UserDetailsService {
+
+    PasswordEncoder passwordEncoder;
+
+    Map<String, AppUser> users = new HashMap<>();
+    DefaultOAuth2UserService oauth2Delegate = new DefaultOAuth2UserService();
+    OidcUserService oidcDelegate = new OidcUserService();
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return users.get(username);
+    }
+
+    @PostConstruct
+    private void createHardcodedUsers() {
+        var bob = AppUser.builder()
+                .username("bob")
+                .provider(LoginProvider.APP)
+                .password(passwordEncoder.encode("1234"))
+                .authorities(List.of(new SimpleGrantedAuthority("read")))
+                .build();
+
+        var bil = AppUser.builder()
+                .username("bil")
+                .provider(LoginProvider.APP)
+                .password(passwordEncoder.encode("321"))
+                .authorities(List.of(new SimpleGrantedAuthority("read")))
+                .build();
+
+        createUser(bob);
+        createUser(bil);
+
+    }
+
+    /**
+     * Adapts oidc login to return AppUser instead of default OidcUser
+     * @return service that returns AppUser from request to the Oidc UserInfo Endpoint
+     */
+    @Bean
+    public OAuth2UserService<OidcUserRequest, OidcUser> oidcLoginHandler() {
+        return userRequest -> {
+            LoginProvider provider = getProvider(userRequest);
+            OidcUser oidcUser = oidcDelegate.loadUser(userRequest);
+            return AppUser
+                    .builder()
+                    .provider(provider)
+                    .username(oidcUser.getEmail())
+                    .name(oidcUser.getFullName())
+                    .email(oidcUser.getEmail())
+                    .userId(oidcUser.getName())
+                    .imageUrl(oidcUser.getAttribute("picture"))
+                    .password(passwordEncoder.encode(UUID.randomUUID().toString()))
+                    .attributes(oidcUser.getAttributes())
+                    .authorities(oidcUser.getAuthorities())
+                    .build();
+        };
+    }
+
+    /**
+     * Adapts oauth2 login to return AppUser instead of default OAauth2User
+     * @return service that returns AppUser from request to the Oauth2 user info
+     */
+    @Bean
+    public OAuth2UserService<OAuth2UserRequest, OAuth2User> oauth2LoginHandler() {
+        return userRequest -> {
+            LoginProvider provider = getProvider(userRequest);
+            OAuth2User oAuth2User = oauth2Delegate.loadUser(userRequest);
+            return AppUser
+                    .builder()
+                    .provider(provider)
+                    .username(oAuth2User.getAttribute("login"))
+                    .name(oAuth2User.getAttribute("login"))
+                    .password(passwordEncoder.encode(UUID.randomUUID().toString()))
+                    .userId(oAuth2User.getName())
+                    .imageUrl(oAuth2User.getAttribute("avatar_url"))
+                    .attributes(oAuth2User.getAttributes())
+                    .authorities(oAuth2User.getAuthorities())
+                    .build();
+        };
+    }
+
+    private LoginProvider getProvider(OAuth2UserRequest userRequest) {
+        return LoginProvider.valueOf(userRequest.getClientRegistration().getRegistrationId().toUpperCase());
+    }
+
+    private void createUser(AppUser user) {
+        users.putIfAbsent(user.getUsername(), user);
+    }
+}
---
+++ src/main/java/com/Authentication/Spring_boot_Account/security/LoginProvider.java
@@ -0,0 +1,5 @@
+package com.Authentication.Spring_boot_Account.security;
+
+public enum LoginProvider {
+    APP, GITHUB, GOOGLE
+}
